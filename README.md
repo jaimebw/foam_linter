9aecd57d62ff9c421946d3b19869328e393df2e2
jaimebw <jaimebwv@gmail.com>
2023-03-30 10:57:20 -0400

    Initial commit

---
+++ README.md
@@ -0,0 +1,22 @@
+# Foam Linter: A simple Open Foam linter
+### WIP
+## How to use
+At the moment, this implementation only works by loading the files as strings.
+Future iterations may change.
+
+A simple example:
+```python3
+from foam_linter import FoamLinter
+
+# file:str
+
+linter = FoamLinter(file)
+result = linter.lint()
+
+result[0] # dictionary of errors
+results[1] # error code, if 1 error if 0 no error
+```
+
+## How to install
+
+WIP
---
+++ tests/test_lib.py
@@ -0,0 +1,43 @@
+from src.main import *
+from pathlib import Path
+
+test_dir = Path("tests/test_files")
+
+def load_data():
+    
+    with open(str(test_dir/"controlDict_badlyclosed"),"r") as f:
+        data_badclosed = f.read()
+    with open(str(test_dir/"controlDict"),"r") as f:
+        data = f.read()
+    return (data,data_badclosed)
+
+
+def test_file_type():
+    """
+    Test for the getting the file type and the class type
+    """
+    data = load_data()[0]
+    linter = FoamLinter(data)
+    assert linter.info["dict_class"] == "dictionary"
+    assert linter.info["dict_type"] == "controlDict"
+
+def test_closed_brackets():
+    data = load_data()
+    #good_file = FoamLinter(data[0])
+    bad_file = FoamLinter(data[1])
+
+    assert bad_file.lint()[1] == 1
+    
+def test_controlDict_lint():
+    """
+    """
+    data = load_data()[0]
+    file_linter = FoamLinter(data)
+    lint_results = file_linter.lint()
+    print(lint_results[0])
+    assert lint_results[1] == 1
+    assert file_linter.info["dict_type"] == "controlDict"
+
+    
+
+
